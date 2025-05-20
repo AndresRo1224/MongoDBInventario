6778358c3630f95c54d70ea8b631691393534412
Andres Rangel <andresrangel8a@gmail.com>
2025-05-20 09:52:55 -0500

    Conexion a mongodb

---
+++ Plantilla-Backend-Node-Mongo/src/controllers/categoria.controllers.js
@@ -0,0 +1,6 @@
+import  Categoria  from "../models/Categorias.js";
+const obtenerCategorias = async (req, res) => {
+    const categorias = await Categoria.find();
+    res.json(categorias);
+};
+export { obtenerCategorias };
---
+++ Plantilla-Backend-Node-Mongo/src/models/Categorias.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+const categoriaSchema = new mongoose.Schema({
+        Imagen :{
+            type: String,
+            require: true,
+            trim: true   
+        },
+        categoriaNombre :{
+            type: String,
+            require: true,
+            trim: true  
+        },
+        Descripcion :{
+            type: String,
+            require: true,
+            trim: true  
+        }
+},
+{
+    timestamps: true
+}
+
+);
+const Categoria = mongoose.model('Categoria', categoriaSchema);
+export default Categoria;
