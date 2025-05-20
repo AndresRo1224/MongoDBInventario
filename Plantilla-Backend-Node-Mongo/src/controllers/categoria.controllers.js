206b5030ed05c3537cb32278dd65d56b37a89f72
Andres Rangel <andresrangel8a@gmail.com>
2025-05-20 10:09:13 -0500

    Conexion Correcta con inventario

--- Plantilla-Backend-Node-Mongo/src/controllers/categoria.controllers.js
+++ Plantilla-Backend-Node-Mongo/src/controllers/categoria.controllers.js
@@ -1,6 +1,7 @@
-import  Categoria  from "../models/Categorias.js";
+import Categoria from "../models/Categorias.js";
 const obtenerCategorias = async (req, res) => {
     const categorias = await Categoria.find();
+    console.log("Categorias encontradas:", categorias); // <-- Agrega este log
     res.json(categorias);
 };
 export { obtenerCategorias };
--- Plantilla-Backend-Node-Mongo/src/models/Categorias.js
+++ Plantilla-Backend-Node-Mongo/src/models/Categorias.js
@@ -1,25 +1,28 @@
+// filepath: d:\Usuario\Desktop\universidad\cloud-computing\Plantilla-Backend-Node-Mongo\Plantilla-Backend-Node-Mongo\src\models\Categorias.js
 import mongoose from 'mongoose';
 const categoriaSchema = new mongoose.Schema({
-        Imagen :{
-            type: String,
-            require: true,
-            trim: true   
-        },
-        categoriaNombre :{
-            type: String,
-            require: true,
-            trim: true  
-        },
-        Descripcion :{
-            type: String,
-            require: true,
-            trim: true  
-        }
-},
-{
+    CategoriaID: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    CategoriaNombre: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    Descripcion: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    Imagen: {
+        type: String,
+        required: true,
+        trim: true
+    }
+}, {
     timestamps: true
-}
-
-);
-const Categoria = mongoose.model('Categoria', categoriaSchema);
+});
+const Categoria = mongoose.model('categorias', categoriaSchema); // nombre igual a la colección
 export default Categoria;
